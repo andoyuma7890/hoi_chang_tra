24d306cc181cb317fc288370a3ae034a5936fd0a
andoyuma7890 <andoyuma7890@github.com>
2024-10-25 10:46:57 +0700

    init

---
+++ b-i-l-i-b-i-l-i-m-a-n-g-a.meta.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         b-i-l-i-b-i-l-i-m-a-n-g-a
+// @namespace    b-i-l-i-b-i-l-i-m-a-n-g-a.user.js
+// @version      2024.10.25
+// @description  down b-i-l-i-b-i-l-i-m-a-n-g-a
+// @author       You
+// @include      /^https:\/\/m[a4]ng[a4]\.b[i1]lib[i1]l[i1]\.c[o0]m\/mc.+?\/.+?/
+// @icon         https://raw.githubusercontent.com/andoyuma7890/hoi_chang_tra/main/b-i-l-i-b-i-l-i-m-a-n-g-a.png
+// @grant        GM_download
+// @downloadURL  https://github.com/andoyuma7890/hoi_chang_tra/raw/main/b-i-l-i-b-i-l-i-m-a-n-g-a.user.js
+// @updateURL    https://github.com/andoyuma7890/hoi_chang_tra/raw/main/b-i-l-i-b-i-l-i-m-a-n-g-a.meta.js
+// ==/UserScript==
