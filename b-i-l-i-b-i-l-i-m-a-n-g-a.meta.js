06eb093b3bc9d9a99e7ee52612713122068f048e
andoyuma7890 <andoyuma7890@github.com>
2024-10-26 13:05:26 +0700

    up

--- b-i-l-i-b-i-l-i-m-a-n-g-a.meta.js
+++ b-i-l-i-b-i-l-i-m-a-n-g-a.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         b-i-l-i-b-i-l-i-m-a-n-g-a
 // @namespace    b-i-l-i-b-i-l-i-m-a-n-g-a.user.js
-// @version      2024.10.25
+// @version      2024.10.26
 // @description  down b-i-l-i-b-i-l-i-m-a-n-g-a
 // @author       You
 // @include      /^https:\/\/m[a4]ng[a4]\.b[i1]lib[i1]l[i1]\.c[o0]m\/mc.+?\/.+?/
 // @icon         https://raw.githubusercontent.com/andoyuma7890/hoi_chang_tra/main/b-i-l-i-b-i-l-i-m-a-n-g-a.png
-// @grant        GM_download
+// @grant        none
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.48/dist/zip.min.js
 // @downloadURL  https://github.com/andoyuma7890/hoi_chang_tra/raw/main/b-i-l-i-b-i-l-i-m-a-n-g-a.user.js
 // @updateURL    https://github.com/andoyuma7890/hoi_chang_tra/raw/main/b-i-l-i-b-i-l-i-m-a-n-g-a.meta.js
 // ==/UserScript==
